4d9c42c3504014bc6a3bc6a256b7e6854ef04aa9
Justin <justinb@live.ca>
2020-09-14 11:36:49 -0400

    update git lines location

--- Git/git-info.sh
+++ Git/git-info.sh
@@ -1,4 +1,13 @@
-# Info related to git history
+# Info related to git repo
 
 # Show time & date of a commit
 git show -s —format=%ci b51bf6b
+
+### Count the lines of code in a git repo
+>_Can exclude folders or file types in the pipes_
+
+```sh
+git ls-files | grep -Ev \
+'node_mudules|.vs|.jpg|.png|.lock|.jar|.bat|.pdf|.otf|.woff' \
+| xargs wc -l
+```
